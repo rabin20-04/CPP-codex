7e7a2651c53df3eae559013bc9a092b9b6e86a44
Rabin Poudel <poudelrabin2004@gmail.com>
2024-05-15 22:18:16 +0545

    5.1

--- 5_prime.cpp
+++ 5_prime.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 int main()
 {
-    int n, count = 0;
+    int n, flag = 0;
     cout << "enter a number  " << endl;
     cin >> n;
     for (int i = 2; i < sqrt(n); i++)
     {
         if (n % i == 0)
         {
-            count++;
+            flag++;
             break;
         }
     }
-    if (count)
+    if (flag)
         cout << "not prime" << endl;
     else
         cout << "prime" << endl;
