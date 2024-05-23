995156c3590af32bf252a3150eddbb2e00664112
Rabin <poudelrabin2004@gmail.com>
2024-05-23 23:27:59 +0545

    15

---
+++ 15_arays.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5};
+    for (int element : a)
+    {
+        cout << element << "\n ";
+    }
+
+    // cout << " Enter " << a[1, 0] << endl;
+    return 0;
+}
