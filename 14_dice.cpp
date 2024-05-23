56b9b2c54332145314f07e80936f44d4a1feed53
Rabin <poudelrabin2004@gmail.com>
2024-05-23 14:22:11 +0545

    14.1

---
+++ 14_dice.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Guess a number between 1-6 " << endl;
+
+    srand(time(NULL));
+    while (true)
+    {
+        cin >> num;
+        if (num < 1 || num > 6)
+        {
+            cout << "Enter between 1 and 6 " << endl;
+            continue;
+        }
+        int digit = (rand() % 6) + 1;
+        if (digit == num)
+        {
+            cout << "Perfect guess! You are genius " << endl;
+            break;
+        }
+        else
+            cout << "Ahh ! wrong guess :(" << "\n It was " << digit << "\nLet's try again" << endl;
+    }
+    return 0;
+}
