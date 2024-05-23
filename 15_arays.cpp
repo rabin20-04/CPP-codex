60f4dabb09939e80817fbfd98de4ad22ef0c469e
Rabin <poudelrabin2004@gmail.com>
2024-05-23 23:31:26 +0545

    15.1

--- 15_arays.cpp
+++ 15_arays.cpp
@@ -3,11 +3,17 @@ using namespace std;
 int main()
 {
     int a[] = {1, 2, 3, 4, 5};
+    // two methods to print entire array
     for (int element : a)
     {
-        cout << element << "\n ";
+        cout << element << " ";
     }
-
+    cout << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
     // cout << " Enter " << a[1, 0] << endl;
     return 0;
 }
