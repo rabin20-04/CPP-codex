45661ad43648f0a9eab929bcc2fe458c9d1280d6
Rabin <poudelrabin2004@gmail.com>
2024-05-23 23:58:32 +0545

    15

--- 15_arays.cpp
+++ 15_arays.cpp
@@ -4,16 +4,65 @@ int main()
 {
     int a[] = {1, 2, 3, 4, 5};
     // two methods to print entire array
+
+    // 1
+
     for (int element : a)
     {
         cout << element << " ";
     }
     cout << endl;
+
+    // 2
+
     for (int i = 0; i < 5; i++)
     {
         cout << a[i] << " ";
     }
     cout << endl;
-    // cout << " Enter " << a[1, 0] << endl;
+
+    // 3
+    for (int i = 0; i < sizeof(a) / sizeof(int); i++)
+    {
+        cout << a[i] << " ";
+    }
+#include <iostream>
+    using namespace std;
+    int main()
+    {
+        int a[] = {1, 2, 3, 4, 5};
+        // two methods to print entire array
+
+        // 1
+
+        for (int element : a)
+        {
+            cout << element << " ";
+        }
+        cout << endl;
+
+        // 2
+
+        for (int i = 0; i < 5; i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+
+        // 3
+        for (int i = 0; i < sizeof(a) / sizeof(int); i++)
+        {
+            cout << a[i] << " ";
+        }
+        // Ithis will print unnecessary message as the size size of array will be equal to 20 in this case
+         //           a[] = {1, 2, 3, 4, 5}I
+        for (int i = 0; i < sizeof(a); i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+        return 0;
+    }
+    cout << endl;
     return 0;
 }
