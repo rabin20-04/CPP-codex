e59ba6e46f831b2afbe1e611c080ee7ed2e1c2e3
Rabin <poudelrabin2004@gmail.com>
2024-06-09 11:29:02 +0545

    25.2

--- 25struct_to_function.cpp
+++ 25struct_to_function.cpp
@@ -10,7 +10,9 @@ struct
     float salary;
 } s1;
 
-void display_info(information s2);
+void display_info(information s2);                // Display by copying
+void display_info_with_original(information &s3); // Plays with the original values
+
 int main()
 {
 
@@ -22,11 +24,27 @@ int main()
     cin >> s1.position;
     cout << "Enter employee salary   " << endl;
     cin >> s1.salary;
+    cout << &s1.employee_name << endl;
     display_info(s1);
+    display_info_with_original(s1);
     return 0;
 }
+// here the new function is making copy of the struct
+
 void display_info(information s2)
 {
-    cout << "\n Employee detail" << "\n Employee name : " << s2.employee_name << "\n Id : "
+    cout << endl
+         << "\n Copy\n Employee detail" << "\n Employee name : " << s2.employee_name << "\n Id : "
          << s2.id << "\n Position : " << s2.position << "\n Salary : " << s2.salary << endl;
+    cout << &s2.employee_name;
+}
+
+// woking with the original struct rather than copying
+
+void display_info_with_original(information &s3)
+{
+    cout << endl
+         << "\n Original\n Employee detail" << "\n Employee name : " << s3.employee_name << "\n Id : "
+         << s3.id << "\n Position : " << s3.position << "\n Salary : " << s3.salary << endl;
+    cout << &s3.employee_name << endl;
 }
