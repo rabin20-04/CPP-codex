1d8f25617c064f2ffba47d04dc1135e667fb9258
Rabin <poudelrabin2004@gmail.com>
2024-07-17 15:05:28 +0545

    to final

--- OOp/Friend/Firend_function.cpp
+++ OOp/Friend/Firend_function.cpp
@@ -5,14 +5,41 @@ class Complex
 {
 private:
     float real, imag;
-  Complex add(Complex,Complex);
+    friend Complex add(Complex, Complex);
 
+public:
+    void get_value()
+    {
+
+        cout << "Enter real part:  ";
+        cin >> real;
+        cout << "Enter imaginary  part:  ";
+        cin >> imag;
+    }
+    void showvalue()
+    {
+        cout << "(" << real << "+" << imag << "i)";
+    }
 };
+Complex add(Complex cc1, Complex cc2)
+{
+    Complex temp;
+    temp.real = cc1.real + cc2.real;
+    temp.imag = cc1.imag + cc2.imag;
+    return temp;
+}
 
 int main()
 {
-
-    cout << "Enter " << endl;
+    Complex c1, c2, c3;
+    c1.get_value();
+    c2.get_value();
+    c3 = add(c1, c2);
+    c1.showvalue();
+    cout << "+";
+    c2.showvalue();
+    cout << "=";
+    c3.showvalue();
 
     return 0;
 }
