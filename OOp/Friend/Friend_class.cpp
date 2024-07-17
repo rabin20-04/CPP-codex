a951c6901e604e1e9c6f179dd88d93619900ae82
Rabin <poudelrabin2004@gmail.com>
2024-07-17 15:45:17 +0545

    friend_class

--- OOp/Friend/Friend_class.cpp
+++ OOp/Friend/Friend_class.cpp
@@ -1,10 +1,55 @@
 #include <iostream>
 using namespace std;
 
+class B;
+
+class A
+{
+private:
+    char passA[20];
+
+public:
+    void getdata()
+    {
+        cout << "\n Enter password of a: ";
+        cin >> passA;
+    }
+    void showdata(B);
+    friend class B;
+};
+
+class B
+{
+private:
+    char passB[20];
+
+public:
+    void getdata()
+    {
+        cout << "\n Enter password of b: ";
+        cin >> passB;
+    }
+    void showdata(A a)
+    {
+        cout << "\n A's password is: " << a.passA << endl;
+    }
+    friend class A;
+};
+void A::showdata(B b)
+{
+    cout << "\n-------------------------------------" << endl;
+    cout << " B's password: " << b.passB << endl;
+}
+
 int main()
 {
 
-    cout << "Enter " << endl;
+    A a;
+    B b;
+    a.getdata();
+    b.getdata();
+    a.showdata(b);
+    b.showdata(a);
 
     return 0;
 }
