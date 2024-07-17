526dfc3da9b78d4600b507a897ebf1bd3219ec81
Rabin <poudelrabin2004@gmail.com>
2024-07-17 12:35:44 +0545

    const

--- OOp/Constants/const_as_data_member.cpp
+++ OOp/Constants/const_as_data_member.cpp
@@ -4,15 +4,30 @@ using namespace std;
 class Count
 {
 private:
-    int input_data;
+    const int input_data;
 
 public:
-    Count(int a) : input_data(a){}
+    Count() : input_data(5) {}
+    Count(int a) : input_data(a) {}
+    void display()
+    {
+        cout << input_data << endl;
+    }
+    int returndata()
+    {
+        return input_data;
+    }
 };
 
 int main()
 {
-    int a;
-
+    Count c1(9), c2;
+    int num;
+    cout << "Enter a number for c3: ";
+    cin >> num;
+    Count c3(num);
+    c1.display();
+    c3.display();
+    cout << "From returndata: " << c2.returndata() << endl;
     return 0;
 }
