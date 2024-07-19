8155d80aa987734c4e225c339b82e3d34ba18e09
Rabin <poudelrabin2004@gmail.com>
2024-07-20 00:43:06 +0545

    friend_class

--- Vectors/understanding_vector.cpp
+++ Vectors/understanding_vector.cpp
@@ -1,15 +1,36 @@
 
-// Vectors comes empty by default
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
+// int main()
+// {
+//     //--------------------------------- Vectors comes empty by default
+//     vector<int> primes; // so we must specify the data type
+//     // primes.push_back(2);
+//     // primes.push_back(3);
+//     // primes.push_back(4);
+//     // primes.push_back(5);
+
+//     cout << "Enter " << primes.size() << endl;
+
+//     return 0;
+// }
+
+//--------output will be zero
+
+//-----------------------------------------------------------------------------------------
+
 int main()
 {
     vector<int> primes;
+    primes.push_back(2);
+    primes.push_back(3);
+    primes.push_back(4);
+    primes.push_back(5);
 
-    cout << "Enter " << endl;
-
+    cout << "Enter " << primes.size() << endl;  //---- to get the number of elements in the vector  use .size()
+    cout << "Enter " << sizeof(primes) << endl; //--- size of gives size of the data type not of the array
     return 0;
 }
