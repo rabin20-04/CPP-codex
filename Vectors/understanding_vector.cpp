d730a790ca0bbe4c7af868be9369f378f206d2b7
Rabin <poudelrabin2004@gmail.com>
2024-07-20 00:04:04 +0545

    vector

---
+++ Vectors/understanding_vector.cpp
@@ -0,0 +1,15 @@
+
+// Vectors comes empty by default
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> primes;
+
+    cout << "Enter " << endl;
+
+    return 0;
+}
