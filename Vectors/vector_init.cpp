05c8418adf57563728bce8f35e1b517a72016835
Rabin <poudelrabin2004@gmail.com>
2024-07-20 14:09:48 +0545

    vector

--- Vectors/vector_init.cpp
+++ Vectors/vector_init.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
+    vector<int> vec1 = {1, 2, 3, 4, 5};
+    vector<int> vec2{1, 2, 3, 4, 5};
+    cout << "Vec1 = {";
+    for (int i = 0; i < vec1.size(); i++)
+    {
 
-    cout << "Enter " << endl;
+        cout << vec1[i] << ",";
+    }
+    cout << "}\n";
+    cout << "Vec2 = {";
+    for (int i = 0; i < vec2.size(); i++)
+    {
+
+        cout << vec2[i] << ",";
+    }
+    cout << "}\n";
 
     return 0;
 }
