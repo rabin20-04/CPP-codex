495a3c06f4782a2212a5577fcc227cc5a896d9a4
Rabin <poudelrabin2004@gmail.com>
2024-07-20 14:56:54 +0545

    md

--- Vectors/vector_init.cpp
+++ Vectors/vector_init.cpp
@@ -7,7 +7,7 @@ int main()
     vector<int> vec1 = {1, 2, 3, 4, 5};
     vector<int> vec2{11, 22, 33, 44, 55}, vec3;
 
-        cout << "Vec1 = {";
+    cout << "Vec1 = {";
     for (int i = 0; i < vec1.size(); i++)
     {
 
@@ -23,8 +23,20 @@ int main()
     }
     cout << "}\n";
 
+    for (const int &i : vec2)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    for (int &i : vec1)
+    {
+        cout << i << " ";
+    }
     return 0;
 }
 
 // vector<int> vector3(5, 12);
 // Here, 5 is the size of the vector and 12 is the value.
+// So, the vector is equivalent to
+
+// vector<int> vector3 = {12, 12, 12, 12, 12};
