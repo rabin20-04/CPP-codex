80fb6ae7df43f4271da67e1289526b65ae973b00
Rabin <poudelrabin2004@gmail.com>
2024-07-25 02:31:12 +0545

    operator overloadin

---
+++ OOp/operator  overloading/overloading.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class apple
+{
+
+private:
+    int a;
+    string name;
+
+private:
+    void get_data()
+    {
+        cout << "Enter " << endl;
+    }
+};
+
+int main()
+{
+
+    apple a1, b1, c1;
+    // c1 = a1 + b1;
+
+    return 0;
+}
