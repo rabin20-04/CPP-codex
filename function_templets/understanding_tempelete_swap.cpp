afeec4ed964f53b57232ca6689841d437987ee34
Rabin <poudelrabin2004@gmail.com>
2024-08-31 13:12:30 +0545

    added

---
+++ function_templets/understanding_tempelete_swap.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// Function template to swap two values of the same data type
+template <typename T>
+void swapValues(T &a, T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int a = 90, b = 42;
+    swapValues(a, b); // Swaps integers
+    cout << "Swapped integers: " << a << " " << b << endl;
+
+    double x = 9.2, y = 8.6;
+    swapValues(x, y); // Swaps doubles
+    cout << "Swapped doubles: " << x << " " << y << endl;
+
+    string p = "hello", q = "world";
+    swapValues(p, q); // Swaps strings
+    cout << "Swapped strings: " << p << " " << q << endl;
+
+    return 0;
+}
